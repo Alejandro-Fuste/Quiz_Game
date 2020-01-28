4151a9ae3a080e985da650d51e722a956827040a
Alejandro F <afuste7@gmail.com>
2020-01-28 18:41:22 -0500

    added functions and array containing quiz objects

--- script.js
+++ script.js
@@ -12,6 +12,41 @@ $(document).ready(function() {
 	const startBtnText = 'Start Quiz';
 	let secondsLeft = 76;
 
+	let quiz = [
+		{
+			question: 'Commonly used data types DO NOT include:',
+			answers: [ 'strings', 'alerts', 'boolean', 'float' ],
+			correct: 'alerts'
+		},
+
+		{
+			question: 'The condition of an if/else statement is enclosed with ',
+			answers: [ 'square brackets', 'parentheses', 'curly braces', 'commas' ],
+			correct: 'parentheses'
+		},
+
+		{
+			question: 'Arrays in javascripts can used to store ',
+			answers: [ 'numbers', 'strings', 'objects', 'all of the above' ],
+			correct: 'all of the above'
+		},
+
+		{
+			question: 'Strings must be enclosed with which of the following except ',
+			answers: [ 'curly braces', 'single quotes', 'double quotes', 'back quotes' ],
+			correct: 'curly braces'
+		},
+
+		{
+			question: 'How do you call a function named "myFunction"?',
+			answers: [ 'call function myFunction()', 'call myFunction()', 'myFunction()', 'myfunction()' ],
+			correct: 'myFunction()'
+		}
+	];
+
+	let counter = quiz.length;
+
+	console.log(quiz);
 	// Function declarations
 
 	function startScreen() {
@@ -46,6 +81,45 @@ $(document).ready(function() {
 		var wrongScore = 75 - secondsLeft;
 
 		console.log(score);
+	}
+
+	function rightAnswers() {
+		var correctAnswers = [];
+
+		for (i = 0; i < quiz.length; i++) {
+			correctAnswers.push(quiz[i].correct);
+		}
+
+		return correctAnswers;
+	}
+
+	function answerResponse() {
+		/* get the text from the answer choice the user clicked
+			var answerChoice = (selector).text
+			&
+			compare it to the list of correct answers
+			
+			var choice = correctAnswers.includes(answerChoice);
+		  
+		  if (choice === true) {
+			print 'Correct!'
+			} else {
+			print 'Wrong!'
+			}
+		  */
+	}
+
+	function nextQuestion() {
+		/* counter--;
+		
+		if (counter > 0){
+		  display next question and answer choices;
+		  
+		  quiz[counter].question;
+		} else {
+		  
+		  resultsPage();
+		} */
 	}
 
 	// Event listener for Start Button
