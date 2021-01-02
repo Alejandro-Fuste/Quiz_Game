011e3030485e21a36869fadc8bb3bf51dbe718f0
Alejandro F <afuste7@gmail.com>
2021-01-02 15:28:53 -0500

    added append method to forEach loop of scores.js file

--- js/scores.js
+++ js/scores.js
@@ -2,7 +2,7 @@
 
 function printHighScores() {
 	// get scores from localStorage or set empty array
-	let hScores = JSON.parse(window.localStorage.getItem('scores')) || [];
+	let hScores = JSON.parse(window.localStorage.getItem('scores'));
 
 	// sort scores from most to least
 	hScores.sort(function(a, b) {
@@ -10,5 +10,10 @@ function printHighScores() {
 	});
 	console.table(hScores);
 	// loop through each score to create an li
+	const highScoreOl = $('#highScoreCard ol');
+	hScores.forEach(function(scores) {
+		let liTag = $('<li>').html(`${scores.name} - ${scores.finalScore}`);
+		highScoreOl.append(liTag);
+	});
 	// display on page my appending
 }
