ff012fd8ff96cca42ef1a7b39cad1efab8febc3c
Alejandro F <afuste7@gmail.com>
2021-01-02 14:16:36 -0500

    added code to first pseudocode comment

--- js/scores.js
+++ js/scores.js
@@ -2,6 +2,8 @@
 
 function printHighScores() {
 	// get scores from localStorage or set empty array
+	let highscores = JSON.parse(window.localStorage.getItem('scores')) || [];
+	console.table(highscores);
 	// sort scores from most to least
 	// loop through each score to create an li
 	// display on page my appending
