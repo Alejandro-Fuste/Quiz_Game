256200e367d4e1d17a42b826f3b140d12d3f99e6
Alejandro F <afuste7@gmail.com>
2021-01-02 14:12:08 -0500

    added pseudocode to printHighscores function

--- js/scores.js
+++ js/scores.js
@@ -1,3 +1,8 @@
 /* file to hold code for scores */
 
-function printHighScores() {}
+function printHighScores() {
+	// get scores from localStorage or set empty array
+	// sort scores from most to least
+	// loop through each score to create an li
+	// display on page my appending
+}
