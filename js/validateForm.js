40f6377fb28b013f1af437da0978c095b610397d
Alejandro F <afuste7@gmail.com>
2021-01-03 13:56:15 -0500

    added first guard clause

--- js/validateForm.js
+++ js/validateForm.js
@@ -6,12 +6,17 @@ const allDoneInput = $('.form-control');
 // Create object to hold error messages
 const errorMessages = { blank: "Input field can't be left blank", characters: 'Input must only contain letters' };
 
+// // Create array to hold error types
+// const errorTypes = ['', ]
+
 // Validate Input Function
 function validateForm() {
 	// Get value from input
 	let name = allDoneInput.val().trim();
 
 	// Guard clause to check for blank value & return error message
+	if ([ '', null, undefined ].includes(name)) {
+	}
 
 	// Guard clause to check for valid input & return error message
 }
