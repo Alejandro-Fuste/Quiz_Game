3e0824a04f61b3634a6b0a3fa469d9b5d0e3d46c
Alejandro F <afuste7@gmail.com>
2021-01-03 13:40:25 -0500

    added pseudocode to function

--- js/validateForm.js
+++ js/validateForm.js
@@ -2,10 +2,16 @@
 
 // Get Input Selector
 const allDoneInput = $('.form-control');
-let name = allDoneInput.val().trim();
 
 // Create object to hold error messages
 const errorMessages = { blank: "Input field can't be left blank", characters: 'Input must only contain letters' };
 
 // Validate Input Function
-function validateForm() {}
+function validateForm() {
+	// Get value from input
+	let name = allDoneInput.val().trim();
+
+	// Guard clause to check for blank value & return error message
+
+	// Guard clause to check for valid input & return error message
+}
