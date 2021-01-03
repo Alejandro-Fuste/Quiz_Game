f899b036cb89ce0528df7375646588736c863da0
Alejandro F <afuste7@gmail.com>
2021-01-03 13:30:12 -0500

    added selector to file

--- js/validateForm.js
+++ js/validateForm.js
@@ -1,6 +1,8 @@
 /* Validate Input from All Done Page */
 
 // Get Input Selector
+const allDoneInput = $('.form-control');
+let name = allDoneInput.val().trim();
 
 // Create object to hold error messages
 
