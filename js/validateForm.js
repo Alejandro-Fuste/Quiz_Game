49c0697dd6d6a37215d893a4b6e41104342b9b03
Alejandro F <afuste7@gmail.com>
2021-01-03 13:34:06 -0500

    added object to hold error messages

--- js/validateForm.js
+++ js/validateForm.js
@@ -5,5 +5,6 @@ const allDoneInput = $('.form-control');
 let name = allDoneInput.val().trim();
 
 // Create object to hold error messages
+const errorMessages = { blank: "Input field can't be left blank", characters: 'Input must only contain letters' };
 
 // Validate Input Function
