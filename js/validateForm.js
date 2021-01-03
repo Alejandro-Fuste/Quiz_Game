80eb03669ea9bea9ab76e41dea399d248a2adc68
Alejandro F <afuste7@gmail.com>
2021-01-03 14:53:32 -0500

    added more styling values

--- js/validateForm.js
+++ js/validateForm.js
@@ -8,7 +8,12 @@ const errorMessageDiv = $('#errorMessage');
 const errorMessages = { blank: "Input field can't be left blank", characters: 'Input must only contain letters' };
 
 // Create object to styling for error messages
-const errorStyles = { display: 'block', border: '5px solid red' };
+const errorStyles = {
+	display: 'inline',
+	border: '3px solid rgb(235, 78, 78)',
+	color: 'rgb(235, 78, 78)',
+	padding: '10px'
+};
 
 // Validate Input Function
 function validateForm() {
