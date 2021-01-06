a2f9c5ca65113beb87efaf4d9e4d4c14dc15ebe4
Alejandro F <afuste7@gmail.com>
2021-01-06 18:12:15 -0500

    removed function and variables that were not being used

--- js/script.js
+++ js/script.js
@@ -19,10 +19,6 @@ $(document).ready(function() {
 	const goBackButton = $('#button-addon2');
 	const errorMessageDiv = $('#errorMessage');
 
-	const cardInstructions =
-		'The quiz will begin once the start button is clicked. You will have 75 seconds to complete the quiz. Questions that are answered incorrectly will result in the time being reduced. When questions are answered correctly, the time it took to answer question will be recorded. Your total time will be displayed at the end of the quiz.';
-	const startBtnText = 'Start Quiz';
-
 	// State variables
 	let [ secondsLeft, score, finalScore, highscores, counter, timerInterval ] = [ 76, [], '', [], 0 ];
 
@@ -38,13 +34,6 @@ $(document).ready(function() {
 	};
 
 	// Function declarations
-
-	function startScreen() {
-		cardTitleEl.text('Code Quiz Challenge');
-		$('.card-body p').before(cardInstuctionsEl.detach());
-		cardTextEl.text(cardInstructions);
-		$('.card-body').append(startBtn.detach());
-	}
 
 	function emptyDiv() {
 		cardTitleEl.empty();
